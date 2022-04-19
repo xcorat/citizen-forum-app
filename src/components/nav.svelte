09321db92f5070caa16988608f6ddf4a6b787bef
Sachi Wagaarachchi <xcorat@gmail.com>
2022-04-19 06:07:15 -0700

    Updated navbar drawer

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -12,25 +12,30 @@
 </script>
 
 
-<div class="navbar bg-base-100">
-    <div class="navbar-start ">
-        <div class="w-10 mr-5">
-            <img src="/images/cflogo1.png" alt="cf logo"/>
-        </div>
-        <a href="/" class="normal-case text-xl">{$_('app_title')}</a>
+<!-- Navbar -->
+<div class="w-full navbar bg-base-100">
+    <div class="flex-none lg:hidden">
+        <label for="nav-drawer-1" class="btn btn-square btn-ghost">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+        </label>
+    </div> 
+    <div class="w-10 mr-5">
+        <img src="/images/cflogo1.png" alt="cf logo"/>
     </div>
-    <div class="navbar-center flex">
-      <ul class="menu menu-horizontal p-0">
-        <li><a href="/form">{$_('nav.fill')}</a></li>
-        <li><a href="/responses">{$_('nav.read_responses')}</a></li>
-      </ul>
+    <a href="/" class="normal-case text-xl">{$_('app_title')}</a>
+    <div class="flex-none hidden lg:block">
+        <ul class="menu menu-horizontal p-0">
+            <li><a href="/form">{$_('nav.fill')}</a></li>
+            <li><a href="/responses">{$_('nav.read_responses')}</a></li>
+        </ul>
     </div>
     <div class="navbar-end">
         <div class="btn-group">
             {#each locales as loc}
                 <button class="btn" class:btn-active="{loc.name == $locale}"
-                 on:click={() => $locale = loc.name}>{loc.label}</button>
+                    on:click={() => $locale = loc.name}>{loc.label}</button>
             {/each}
         </div>
     </div>
 </div>
+    
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,11 +1,31 @@
 <script>
 	import '../app.css';
+
+    import { dictionary, locale, _ } from 'svelte-i18n';
+
 	import '../lib/i18n';
     import Nav from "../components/nav.svelte"
 </script>
 
 
+<!--
+	TODO: The drawer doesn't close after clicking th second time. Debug?
+-->
 <div class='max-w-6xl mx-auto bg-base-100 min-h-screen'>
-    <Nav/>
-    <slot></slot>
+	<div class="drawer">
+		<input id="nav-drawer-1" type="checkbox" class="drawer-toggle"> 
+		<div class="drawer-content flex flex-col">
+			<Nav/>
+			<slot></slot>
+		</div>
+		<div class="drawer-side">
+			<label for="nav-drawer-1" class="drawer-overlay"></label> 
+			<ul class="menu p-4 overflow-y-auto w-80 bg-base-100">
+				<!-- Sidebar content here -->
+				<li><a href="/form">{$_('nav.fill')}</a></li>
+				<li><a href="/responses">{$_('nav.read_responses')}</a></li>
+			</ul>
+		</div>
+			
+	</div>
 </div>
