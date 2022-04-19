aca3c26f376b64ec81563c8b16f3fdf28871523f
Sachi Wagaarachchi <xcorat@gmail.com>
2022-04-19 05:42:23 -0700

    Updated navbar styling

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -13,13 +13,13 @@
 
 
 <div class="navbar bg-base-100">
-    <div class="flex-1 navbar-start ">
+    <div class="navbar-start ">
         <div class="w-10 mr-5">
             <img src="/images/cflogo1.png" alt="cf logo"/>
         </div>
         <a href="/" class="normal-case text-xl">{$_('app_title')}</a>
     </div>
-    <div class="flex-none">
+    <div class="navbar-center flex">
       <ul class="menu menu-horizontal p-0">
         <li><a href="/form">{$_('nav.fill')}</a></li>
         <li><a href="/responses">{$_('nav.read_responses')}</a></li>
