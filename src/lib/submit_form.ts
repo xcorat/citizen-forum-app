7470f6fd75dfda3b3b475a4e305ade50a03cb487
Sachi Wagaarachchi <xcorat@gmail.com>
2022-05-12 15:18:39 -0700

    submit google form implemented as a netlify function

--- src/lib/submit_form.ts
+++ src/lib/submit_form.ts
@@ -1,10 +1,16 @@
+import { form } from "svelte-forms";
+
 const googleFormAddrDeploy = import.meta.env.VITE_GOOGLE_FORM_ADDR_DEPLOY as string;
 const googleFormAddrDebug = import.meta.env.VITE_GOOGLE_FORM_ADDR_DEBUG as string;
-const googlekey = import.meta.env.VITE_GAPI_KEY as string
+const googlekey = import.meta.env.VITE_GAPI_KEY as string  
 
-function submitGoogleForm(form_summary) {
+async function submitGoogleForm(form_summary) {
+    //submitGoogleFormold(form_summary)
+    //return;
+
     const url = "/api/submit_form"
-    async function post_form () {
+    console.log(form_summary)
+    async function post_form (form_summary) {
 		const res = await fetch(url, {
 			method: 'POST',
 			body: JSON.stringify(form_summary)
@@ -13,6 +19,7 @@ function submitGoogleForm(form_summary) {
 		const json = await res.json()
 		console.log(JSON.stringify(json))
 	}
+    await post_form(form_summary);
 }
 
 export default submitGoogleForm;
