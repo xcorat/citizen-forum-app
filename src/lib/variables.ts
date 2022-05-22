a1a7f0502ba6fba93810356bbef3f8bdf011b5fa
Sachi Wagaarachchi <xcorat@gmail.com>
2022-05-22 00:04:57 -0700

    prettier

--- src/lib/variables.ts
+++ src/lib/variables.ts
@@ -3,14 +3,8 @@ import { dev, browser } from '$app/env'
 function get_gcs_lang() {
     // This shouldn't be called from the browser, so return a dummy
     // TODO: https://github.com/xcorat/citizen-forum-app/issues/5
+    if(browser) return null;
     
-    if(browser) return {
-        project_id:     import.meta.env.VITE_GCS_LANG_PROJECT_ID,
-        client_email:   import.meta.env.VITE_GCS_LANG_CLIENT_EMAIL,
-        private_key:    import.meta.env.VITE_GCS_LANG_PRIVATE_KEY,
-    }
-    
-    console.log(dev)
     const gcs_lang = {
         project_id: process.env.GCS_LANG_PROJECT_ID,
         client_email: process.env.GCS_LANG_CLIENT_EMAIL,
